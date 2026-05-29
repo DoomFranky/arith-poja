0f19018fb992a3f784f15494c6033e4f05d84c5d
Randy <raoulrandy3@gmail.com>
2026-05-29 10:02:44 +0300

    fix!: modifie the format

--- src/main/java/com/example/demo/arith/controller/ArithController.java
+++ src/main/java/com/example/demo/arith/controller/ArithController.java
@@ -2,67 +2,66 @@ package com.example.demo.arith.controller;
 
 import com.example.demo.arith.exception.BadRequestException;
 import com.example.demo.arith.service.ArithService;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
 @RestController
 public class ArithController {
-    private ArithService arithService;
+  private ArithService arithService;
 
-    public ArithController (ArithService arithService) {
-        this.arithService = arithService;
-    }
+  public ArithController(ArithService arithService) {
+    this.arithService = arithService;
+  }
 
-    @GetMapping("/add")
-    public ResponseEntity<?> addTwoNumber (@PathVariable Long a, @PathVariable Long b) {
-        try {
-            BigInteger number = arithService.add(a,b);
-            return ResponseEntity.ok().body(number);
-        } catch (BadRequestException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().body(e.getMessage());
-        }
+  @GetMapping("/add")
+  public ResponseEntity<?> addTwoNumber(@PathVariable Long a, @PathVariable Long b) {
+    try {
+      BigInteger number = arithService.add(a, b);
+      return ResponseEntity.ok().body(number);
+    } catch (BadRequestException e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    } catch (Exception e) {
+      return ResponseEntity.internalServerError().body(e.getMessage());
     }
+  }
 
-    @GetMapping("/minus")
-    public ResponseEntity<?> minusTwoNumber (@PathVariable Long a, @PathVariable Long b) {
-        try {
-            BigInteger number = arithService.minus(a,b);
-            return ResponseEntity.ok().body(number);
-        } catch (BadRequestException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().body(e.getMessage());
-        }
+  @GetMapping("/minus")
+  public ResponseEntity<?> minusTwoNumber(@PathVariable Long a, @PathVariable Long b) {
+    try {
+      BigInteger number = arithService.minus(a, b);
+      return ResponseEntity.ok().body(number);
+    } catch (BadRequestException e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    } catch (Exception e) {
+      return ResponseEntity.internalServerError().body(e.getMessage());
     }
+  }
 
-    @GetMapping("/multiply")
-    public ResponseEntity<?> multiplyTwoNumber (@PathVariable Long a, @PathVariable Long b) {
-        try {
-            BigInteger number = arithService.multiply(a,b);
-            return ResponseEntity.ok().body(number);
-        } catch (BadRequestException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().body(e.getMessage());
-        }
+  @GetMapping("/multiply")
+  public ResponseEntity<?> multiplyTwoNumber(@PathVariable Long a, @PathVariable Long b) {
+    try {
+      BigInteger number = arithService.multiply(a, b);
+      return ResponseEntity.ok().body(number);
+    } catch (BadRequestException e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    } catch (Exception e) {
+      return ResponseEntity.internalServerError().body(e.getMessage());
     }
+  }
 
-    @GetMapping("/divide")
-    public ResponseEntity<?> divideToNumber (@PathVariable Long a, @PathVariable Long b) {
-        try {
-            BigDecimal number = arithService.divide(a,b);
-            return ResponseEntity.ok().body(number);
-        } catch (BadRequestException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().body(e.getMessage());
-        }
+  @GetMapping("/divide")
+  public ResponseEntity<?> divideToNumber(@PathVariable Long a, @PathVariable Long b) {
+    try {
+      BigDecimal number = arithService.divide(a, b);
+      return ResponseEntity.ok().body(number);
+    } catch (BadRequestException e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    } catch (Exception e) {
+      return ResponseEntity.internalServerError().body(e.getMessage());
     }
+  }
 }
